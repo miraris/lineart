fbd4dac977212e9d7f1f7578ccae19b3fb2742d3
Miraris <miraris@autistici.org>
2018-11-24 16:10:24 +0100

    create an api route that returns random img from mongodb
    Signed-off-by: Miraris <miraris@autistici.org>

--- api/index.js
+++ api/index.js
@@ -1,17 +1,28 @@
 const Koa = require('koa');
 const cors = require('@koa/cors');
 const Router = require('koa-router');
+const mongo = require('koa-mongo');
 
 const app = new Koa();
 const router = new Router();
 
-router.get('/', (ctx, next) => {
-  ctx.body = { data: 'henlo' };
+router.get('/', async (ctx) => {
+  const coll = await ctx.mongo.db('lineart').collection('images');
+
+  const n = await coll.estimatedDocumentCount();
+  const r = Math.floor(Math.random() * n);
+
+  ctx.body = await coll
+    .find()
+    .limit(1)
+    .skip(r)
+    .toArray();
 });
 
 app
+  .use(mongo({ uri: 'mongodb://mongo' }))
+  .use(cors({ origin: '*' }))
   .use(router.routes())
-  .use(router.allowedMethods())
-  .use(cors());
+  .use(router.allowedMethods());
 
 app.listen(3000);
